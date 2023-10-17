d1bc619553ce8bf043fcb33e92703f545aee0106
Sale\salle <studenticomdata@outlook.com>
2023-10-17 18:30:18 +0200

    matOption error fixed

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FormsModule} from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
@@ -44,7 +45,8 @@ import {MatSelectModule} from '@angular/material/select';
     MatDatepickerModule,
     MatNativeDateModule,
     MatRadioModule,
-    MatSelectModule
+    MatSelectModule,
+    BrowserAnimationsModule
 
   ],
   providers: [],
