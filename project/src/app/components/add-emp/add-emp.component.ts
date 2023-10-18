481d8a2c8f0d44add8f24470cb99a20f50c10ffd
Sale\salle <studenticomdata@outlook.com>
2023-10-18 15:39:07 +0200

    binding input values and display them in console

--- project/src/app/components/add-emp/add-emp.component.ts
+++ project/src/app/components/add-emp/add-emp.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -8,6 +9,8 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class AddEmpComponent {
 
+  empForm : FormGroup;
+
   education : string[] = [ //niz stringova za zeljeni stepen strucne spreme
     'Elementary Education',
     'Secondary Education',
@@ -15,7 +18,27 @@ export class AddEmpComponent {
     'Bachelors Degree',
     'Masters Degree',
     'Doctorate (PhD)'
-  ]
+  ];
+
+  constructor(private _formBuilder : FormBuilder){ //servis _formBuilder , bindovanje vrednosti sa input poljima
+    this.empForm = this._formBuilder.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      date: '',
+      gender: '',
+      education: '',
+      company: '',
+      experience:'',
+      country:''
+    })
+  };
+
+  onRegister(){
+    if(this.empForm.valid){
+      console.log(this.empForm.value);
+    }
+  }
 
 }
 
