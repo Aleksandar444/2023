0625aa6db803e2cb541c98032e405c34bc76af87
Sale\salle <studenticomdata@outlook.com>
2023-10-27 15:40:21 +0200

    fixing routing

--- project/src/app/components/login/login.component.ts
+++ project/src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component,OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,7 +10,7 @@ import { FormControl } from '@angular/forms';
 export class LoginComponent implements OnInit {
   login:FormGroup<any>;
 
-  constructor(){
+  constructor(private router :Router){
 
   }
   ngOnInit(): void {
@@ -21,5 +22,6 @@ export class LoginComponent implements OnInit {
   onLogin(){
     alert("works");
     console.log(this.login.value);
+    this.router.navigate(['/main-page']);
   }
 }
