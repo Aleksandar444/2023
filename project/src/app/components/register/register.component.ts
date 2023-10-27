159ece4bdbd8f92ab34a851418330c9e5b27c176
Sale\salle <studenticomdata@outlook.com>
2023-10-27 15:34:20 +0200

    register  input fields binding

--- project/src/app/components/register/register.component.ts
+++ project/src/app/components/register/register.component.ts
@@ -1,17 +1,28 @@
 import { Component,OnInit } from '@angular/core';
-
+import { FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent  {
+export class RegisterComponent implements OnInit  {
+  register : FormGroup<any>;
   constructor(){
 
   }
-
+ngOnInit(): void {
+  this.register = new FormGroup({
+    'userNameReg' : new FormControl,
+    'passwordReg' : new FormControl,
+    'email' : new FormControl,
+    'city': new FormControl,
+    'state' : new FormControl
+  })
+}
 
   onRegister(){
     alert("Register works");
+    console.log(this.register.value);
   }
 }
